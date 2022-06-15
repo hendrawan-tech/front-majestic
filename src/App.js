67b62f2b03cac2e6925e09951e23486ecfa3a2e4
Hendrawan <hendrawan.dev@gmail.com>
2022-06-15 13:08:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import CreateDestination from "./pages/destinations/CreateDestination";
+import Destination from "./pages/destinations/Destination";
+import UpdateDestination from "./pages/destinations/UpdateDestination";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/destinations" element={<Destination />} />
+        <Route path="/destinations/create" element={<CreateDestination />} />
+        <Route
+          path="/destinations/update/:id"
+          element={<UpdateDestination />}
+        />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/helpers/Config.js
@@ -0,0 +1,7 @@
+export function apiUrl() {
+  return "http://127.0.0.1:8000/api";
+}
+
+export function appUrl() {
+  return "http://127.0.0.1:8000";
+}
